a92bff6d0332f2f0165a7eb69e9b0eff20a2dc38
Vedant Purohit <vdpurohit13@gmail.com>
2020-05-13 16:04:32 -0400

    Added more detection functions

--- weather_report.py
+++ weather_report.py
@@ -1,7 +1,11 @@
 '''Module to hold functionality related to aviation weather reports.'''
 
+from datetime import datetime
+
 from parser import tokenize_text
 from keywords import TYPE_KEYWORDS
+
+# TO DO: Validating a weather report.
 
 def categorize_report(report):
   '''
@@ -36,3 +40,44 @@ def get_airport_code(report):
     return tokens[1]
 
   return 'Did not find a 4-digit code at the second position of the report.'
+
+def get_time(report):
+  '''
+  This function finds and parses the date in the weather report. Function assumes that the 3rd position
+  is the date position
+
+  params:
+  report: string
+
+  return:
+  {
+    'day': int,
+    'hour': int,
+    'minute': int
+  }
+  '''
+  tokens = tokenize_text(report)
+  time = tokens[2]
+  if len(time) == 7:
+    return {
+      'day': int(time[:2]),
+      'hour': int(time[2:4]),
+      'minute': int(time[4:6])
+    }
+  
+  return 'Did not find a 7-digit time in the weather report.'
+
+def is_auto(report):
+  '''
+  This function determines if a weather report is automated or manual.
+
+  params:
+  report: string
+
+  return:
+  is_auto: boolean.
+  '''
+  tokens = tokenize_text(report)
+  is_auto = tokens[3]
+
+  return is_auto == 'AUTO'
