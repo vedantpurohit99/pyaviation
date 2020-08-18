9b0f57a387359d03dc52a6789b5a15f32438c61a
Vedant Purohit <vdpurohit13@gmail.com>
2020-08-17 21:46:51 -0400

    Added Keywords

--- keywords.py
+++ keywords.py
@@ -5,3 +5,76 @@ TYPE_KEYWORDS = [
   'SPECI'
   'TAF'
 ]
+
+DESCRIPTOR_KEYWORDS = [
+  'MI',
+  'BC',
+  'DR',
+  'BL',
+  'SH',
+  'TS',
+  'PR',
+  'FZ'
+]
+
+DESCRIPTOR_MAP = {
+  'MI': 'Shallow',
+  'BC': 'Patches',
+  'DR': 'Drifting',
+  'BL': 'Blowing',
+  'SH': 'Shower(s)',
+  'TS': 'Thunderstorm',
+  'PR': 'Partial',
+  'FZ': 'Freezing'
+}
+
+PHENOMENA_KEYWORDS = [
+  'DZ',
+  'RA',
+  'SN',
+  'SG',
+  'PL',
+  'GR',
+  'GS',
+  'IC',
+  'UP',
+  'HZ',
+  'FU',
+  'SA',
+  'DU',
+  'FG',
+  'BR',
+  'VA',
+  'PO',
+  'SS',
+  'DS',
+  'SQ',
+  '+FC',
+  'FC'
+]
+
+PHENOMENA_MAP = {
+  'DZ': 'Drizzle',
+  'RA': 'Rain',
+  'SN': 'Snow',
+  'SG': 'Snow Grains',
+  'PL': 'Ice Pellets',
+  'GR': 'Hail',
+  'GS': 'Snow Pellets',
+  'IC': 'Ice Crystals',
+  'UP': 'Unknown Precipitation',
+  'HZ': 'Haze',
+  'FU': 'Smoke',
+  'SA': 'Sand',
+  'DU': 'Dust',
+  'FG': 'Fog',
+  'BR': 'Mist',
+  'VA': 'Volcanic Ash',
+  'PO': 'Dust/Sand Whirls',
+  'SS': 'Sandstorm',
+  'DS': 'Duststorm',
+  'SQ': 'Squalls',
+  '+FC': 'Tornado/Watersprout',
+  'FC': 'Funnel Cloud'
+}
+
